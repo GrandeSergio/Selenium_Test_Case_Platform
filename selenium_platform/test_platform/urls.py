97e05384fa274a0409d7724e78c1884ba8b12d37
adacewicz <arekdacewicz@gmail.com>
2023-02-28 21:20:59 +0100

    2 commit

--- selenium_platform/test_platform/urls.py
+++ selenium_platform/test_platform/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.test_list, name='test_list'),
     path('<int:test_id>/', views.test_details, name='test_details'),
+    path('<int:test_id>/run/', views.run_test_cases, name='run_test_cases'),
     path('upload/', views.test_upload, name='test_upload'),
 ]
--- selenium_platform/test_platform/views.py
+++ selenium_platform/test_platform/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 from .models import TestCase
-from  .forms import TestUploadForm
+from .forms import TestUploadForm
+import subprocess
+from django.middleware.csrf import get_token
+
+
 
 def test_list(request):
     tests = TestCase.objects.all()
     return render(request, 'test_list.html', {'tests': tests})
 
+#def test_details(request, test_id):
+#    test = get_object_or_404(TestCase, pk=test_id)
+#    return render(request, 'test_details.html', {'test': test})
+
 def test_details(request, test_id):
     test = get_object_or_404(TestCase, pk=test_id)
+    if request.method == 'POST':
+        # Run the uploaded script
+        result = subprocess.run(['python', test.file.path], stdout=subprocess.PIPE)
+        output = result.stdout.decode('utf-8')
+        return JsonResponse({'output': output})
     return render(request, 'test_details.html', {'test': test})
+
 
 def test_upload(request):
     if request.method == 'POST':
@@ -20,3 +34,45 @@ def test_upload(request):
     else:
         form = TestUploadForm()
     return render(request, 'test_upload.html', {'form': form})
+
+'''
+def run_test_cases(request):
+    if request.method == 'POST':
+        # Get the uploaded file
+        file = request.FILES.get('file')
+
+        # Check if file is a Python file
+        if file.name.endswith('.py'):
+            # Save the file to a temporary location
+            with open('temp.py', 'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+
+            # Run the file using subprocess module
+            result = subprocess.run(['python', 'temp.py'], capture_output=True, text=True)
+
+            # Get the output of the script
+            output = result.stdout
+
+            # Render the output in a template
+            return render(request, 'test_details.html', {'output': output})
+        else:
+            # Render an error message if file is not a Python file
+            return render(request, 'test_details.html', {'error': 'File must be a Python file.'})
+    else:
+        return render(request, 'test_details.html')
+'''
+def run_test_cases(request, test_id):
+    if request.method == 'POST':
+        test_case = get_object_or_404(TestCase, pk=test_id)
+        # Get the CSRF token
+        csrf_token = get_token(request)
+        # Get the file contents
+        file_contents = test_case.file.read()
+        # Create a new namespace for the test case
+        namespace = {'csrfmiddlewaretoken': csrf_token, 'file_contents': file_contents}
+        # Run the test case
+        result = exec(file_contents, namespace)
+        return HttpResponse(result)
+    else:
+        return ("Only POST requests are allowed")
